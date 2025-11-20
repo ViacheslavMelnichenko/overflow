6e23d9004120be41d434a7c2e2c5014977616803
Viacheslav Melnichenko <melnichenkoviatcheslav@gmail.com>
2025-11-20 03:01:22 +0100

    Add Question and Tags controllers

---
+++ QuestionService/Controllers/QuestionsController.cs
@@ -0,0 +1,115 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QuestionService.Data;
+using QuestionService.DTOs;
+using QuestionService.Models;
+
+namespace QuestionService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class QuestionsController(QuestionDbContext dbContext) : ControllerBase
+{
+    [Authorize]
+    [HttpPost]
+    public async Task<ActionResult<Question>> CreateQuestion(CreateQuestionDto dto)
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var name = User.FindFirstValue("name");
+        if (userId is null || name is null)
+        {
+            return BadRequest("Cannot get user details");
+        }
+
+        var question = new Question
+        {
+            Title = dto.Title,
+            Content = dto.Content,
+            TagSlugs = dto.Tags,
+            Askerid = userId,
+            AskerDisplayName = name
+        };
+
+        dbContext.Questions.Add(question);
+        await dbContext.SaveChangesAsync();
+        return Created($"/questions/{question.Id}", question);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Question>>> GetQuestions(string? tag)
+    {
+        var query = dbContext.Questions.AsQueryable();
+
+        if (!string.IsNullOrWhiteSpace(tag))
+        {
+            query = query.Where(q => q.TagSlugs.Contains(tag));
+        }
+
+        return Ok(await query.OrderBy(q => q.CreatedAt).ToListAsync());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Question>> GetQuestion(string id)
+    {
+        var question = await dbContext.Questions.FindAsync(id);
+
+        if (question is null)
+        {
+            return NotFound();
+        }
+
+        await dbContext.Questions
+            .Where(q => q.Id == id)
+            .ExecuteUpdateAsync(setters => setters.SetProperty(x => x.ViewCount, x => x.ViewCount + 1));
+
+        return question;
+    }
+
+    [Authorize]
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateQuestion(string id, CreateQuestionDto dto)
+    {
+        var question = await dbContext.Questions.FindAsync(id);
+        if (question is null)
+        {
+            return NotFound();
+        }
+
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (question.Askerid != userId)
+        {
+            return Forbid();
+        }
+
+        question.Title = dto.Title;
+        question.Content = dto.Content;
+        question.TagSlugs = dto.Tags;
+        question.UpdatedAt = DateTime.UtcNow;
+
+        await dbContext.SaveChangesAsync();
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteQuestion(string id)
+    {
+        var question = await dbContext.Questions.FindAsync(id);
+        if (question is null)
+        {
+            return NotFound();
+        }
+
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (question.Askerid != userId)
+        {
+            return Forbid();
+        }
+
+        dbContext.Questions.Remove(question);
+
+        await dbContext.SaveChangesAsync();
+        return NoContent();
+    }
+}
---
+++ QuestionService/DTOs/CreateQuestionDto.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuestionService.DTOs;
+
+public record CreateQuestionDto(
+    [Required] string Title,
+    [Required] string Content,
+    [Required] List<string> Tags);
