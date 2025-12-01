09e94989d6f5e92879b0c48b3672c11451413099
Viacheslav Melnichenko <melnichenkoviatcheslav@gmail.com>
2025-12-01 07:27:09 +0100

    fix infra

--- terraform-infra/ingress.tf
+++ terraform-infra/ingress.tf
@@ -99,7 +99,7 @@ resource "kubernetes_ingress_v1" "typesense_dashboard_staging" {
     ingress_class_name = "nginx"
 
     rule {
-      host = "typesense-staging.helios"
+      host = "overflow-typesense-staging.helios"
 
       http {
         path {
@@ -139,7 +139,7 @@ resource "kubernetes_ingress_v1" "typesense_api_endpoint_staging" {
     ingress_class_name = "nginx"
 
     rule {
-      host = "typesense-api-staging.helios"
+      host = "overflow-typesense-api-staging.helios"
 
       http {
         path {
@@ -161,103 +161,6 @@ resource "kubernetes_ingress_v1" "typesense_api_endpoint_staging" {
 
   depends_on = [kubernetes_stateful_set.typesense_staging]
 }
-
-############################
-# INGRESS — STAGING (APPS)
-############################
-
-# resource "kubernetes_ingress_v1" "aspire_staging" {
-#   metadata {
-#     name      = "aspire-staging"
-#     namespace = kubernetes_namespace.apps_staging.metadata[0].name
-#   }
-# 
-#   spec {
-#     ingress_class_name = "nginx"
-# 
-#     rule {
-#       host = "aspire-staging.helios"
-# 
-#       http {
-#         path {
-#           path      = "/"
-#           path_type = "Prefix"
-# 
-#           backend {
-#             service {
-#               name = "aspire-staging"
-#               port {
-#                 number = 18888
-#               }
-#             }
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
-
-# resource "kubernetes_ingress_v1" "questions_api_staging" {
-#   metadata {
-#     name      = "questions-api-staging"
-#     namespace = kubernetes_namespace.apps_staging.metadata[0].name
-#   }
-# 
-#   spec {
-#     ingress_class_name = "nginx"
-# 
-#     rule {
-#       host = "questions-api-staging.helios"
-# 
-#       http {
-#         path {
-#           path      = "/"
-#           path_type = "Prefix"
-# 
-#           backend {
-#             service {
-#               name = "questions-api-staging"
-#               port {
-#                 number = 7001
-#               }
-#             }
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
-
-# resource "kubernetes_ingress_v1" "search_api_staging" {
-#   metadata {
-#     name      = "search-api-staging"
-#     namespace = kubernetes_namespace.apps_staging.metadata[0].name
-#   }
-# 
-#   spec {
-#     ingress_class_name = "nginx"
-# 
-#     rule {
-#       host = "search-api-staging.helios"
-# 
-#       http {
-#         path {
-#           path      = "/"
-#           path_type = "Prefix"
-# 
-#           backend {
-#             service {
-#               name = "search-api-staging"
-#               port {
-#                 number = 7002
-#               }
-#             }
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
 
 ############################
 # INGRESS — PRODUCTION (INFRA)
@@ -329,39 +232,6 @@ resource "kubernetes_ingress_v1" "typesense_dashboard_ui_production" {
   depends_on = [kubernetes_deployment.typesense_dashboard_production]
 }
 
-resource "kubernetes_ingress_v1" "typesense_dashboard_production" {
-  metadata {
-    name      = "typesense-dashboard-production"
-    namespace = kubernetes_namespace.infra_production.metadata[0].name
-  }
-
-  spec {
-    ingress_class_name = "nginx"
-
-    rule {
-      host = "typesense-production.helios"
-
-      http {
-        path {
-          path      = "/"
-          path_type = "Prefix"
-
-          backend {
-            service {
-              name = "typesense-dashboard"
-              port {
-                number = 80
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-
-  depends_on = [kubernetes_deployment.typesense_dashboard_production]
-}
-
 # Typesense API endpoint for dashboard to connect to
 resource "kubernetes_ingress_v1" "typesense_api_endpoint_production" {
   metadata {
@@ -379,7 +249,7 @@ resource "kubernetes_ingress_v1" "typesense_api_endpoint_production" {
     ingress_class_name = "nginx"
 
     rule {
-      host = "typesense-api.helios"
+      host = "overflow-typesense-api.helios"
 
       http {
         path {
@@ -401,101 +271,3 @@ resource "kubernetes_ingress_v1" "typesense_api_endpoint_production" {
 
   depends_on = [kubernetes_stateful_set.typesense_production]
 }
-
-############################
-# INGRESS — PRODUCTION (APPS)
-############################
-
-# resource "kubernetes_ingress_v1" "aspire_production" {
-#   metadata {
-#     name      = "aspire-production"
-#     namespace = kubernetes_namespace.apps_production.metadata[0].name
-#   }
-# 
-#   spec {
-#     ingress_class_name = "nginx"
-# 
-#     rule {
-#       host = "aspire-production.helios"
-# 
-#       http {
-#         path {
-#           path      = "/"
-#           path_type = "Prefix"
-# 
-#           backend {
-#             service {
-#               name = "aspire-production"
-#               port {
-#                 number = 18888
-#               }
-#             }
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
-
-# resource "kubernetes_ingress_v1" "questions_api_production" {
-#   metadata {
-#     name      = "questions-api-production"
-#     namespace = kubernetes_namespace.apps_production.metadata[0].name
-#   }
-# 
-#   spec {
-#     ingress_class_name = "nginx"
-# 
-#     rule {
-#       host = "questions-api-production.helios"
-# 
-#       http {
-#         path {
-#           path      = "/"
-#           path_type = "Prefix"
-# 
-#           backend {
-#             service {
-#               name = "questions-api-production"
-#               port {
-#                 number = 7001
-#               }
-#             }
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
-
-# resource "kubernetes_ingress_v1" "search_api_production" {
-#   metadata {
-#     name      = "search-api-production"
-#     namespace = kubernetes_namespace.apps_production.metadata[0].name
-#   }
-# 
-#   spec {
-#     ingress_class_name = "nginx"
-# 
-#     rule {
-#       host = "search-api-production.helios"
-# 
-#       http {
-#         path {
-#           path      = "/"
-#           path_type = "Prefix"
-# 
-#           backend {
-#             service {
-#               name = "search-api-production"
-#               port {
-#                 number = 7002
-#               }
-#             }
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
-
