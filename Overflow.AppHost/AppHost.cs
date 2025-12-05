43765de36635b4d1475dc81c1da7a4a0dfcb212f
Viacheslav Melnichenko <melnichenkoviatcheslav@gmail.com>
2025-12-05 16:51:01 +0100

    update app host

--- Overflow.AppHost/AppHost.cs
+++ Overflow.AppHost/AppHost.cs
@@ -1,61 +1,80 @@
 using Microsoft.Extensions.Hosting;
-using Overflow.AppHost.Extensions;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-// Keycloak setup
-if (builder.Environment.IsDevelopment())
-{
-    // Local Keycloak container for development
-    var keycloak = builder
-        .AddKeycloak("keycloak", 6001)
-        .WithDataVolume("keycloak-data");
+// var compose = builder.AddDockerComposeEnvironment("production")
+//     .WithDashboard(dashboard => dashboard.WithHostPort(8080));
 
-    // Question Service configuration
-    var questionService = builder.AddProject<Projects.QuestionService>("question-svc")
-        .WithReference(keycloak)
-        .WithKeycloakOptions(builder.Configuration)
-        .WaitFor(keycloak);
+var keycloak = builder.AddKeycloak("keycloak", 6001)
+    .WithDataVolume("keycloak-data")
+    //.WithRealmImport("../infra/realms")
+    .WithEnvironment("KC_HTTP_ENABLED", "true")
+    .WithEnvironment("KC_HOSTNAME_STRICT", "false")
+    .WithEnvironment("VIRTUAL_HOST", "id.overflow.local")
+    .WithEnvironment("VIRTUAL_PORT", "8080");
 
-    var webapp = builder.AddNpmApp("webapp", "../webapp", "dev")
-        .WithReference(keycloak)
-        .WithHttpEndpoint(env: "PORT", port: 3000);
-}
+var postgres = builder.AddPostgres("postgres", port: 5432)
+    .WithDataVolume("postgres-data")
+    .WithPgAdmin();
 
-// var postgres = builder.AddPostgres("postgres", port: 5432)
-//     .WithDataVolume("postgres-data")
-//     .WithPgAdmin();
-//
 // var typesenseApiKey = builder.AddParameter("typesense-api-key", secret: true);
-//
-// var typesense = builder.AddContainer("typesense", "typesense/typesense", "29.0")
-//     .WithArgs("--data-dir", "/data", "--api-key", typesenseApiKey, "--enable-cors")
-//     .WithVolume("typesense-data", "/data")
-//     .WithHttpEndpoint(8108, 8108, name: "typesense");
-//
-// var typesenseContainer = typesense.GetEndpoint("typesense");
-//
-// var questionDb = postgres.AddDatabase("questionDb");
-//
-// var rabbitmq = builder.AddRabbitMQ("messaging")
-//     .WithDataVolume("rabbitmq-data")
-//     .WithManagementPlugin(port: 15672);
-//
-// var questionService = builder.AddProject<Projects.QuestionService>("question-svc")
-//     .WithEnvironment("KEYCLOAK_HTTP", keycloakHttp)
-//     .WithEnvironment("KEYCLOAK_MANAGEMENT", keycloakMgmt)
-//     .WithReference(questionDb)
-//     .WithReference(rabbitmq)
-//     .WaitFor(keycloak)
-//     .WaitFor(questionDb)
-//     .WaitFor(rabbitmq);
-//
-//
-// var searchService = builder.AddProject<Projects.SearchService>("search-svc")
-//     .WithEnvironment("typesense-api-key", typesenseApiKey)
-//     .WithReference(typesenseContainer)
-//     .WithReference(rabbitmq)
-//     .WaitFor(typesense)
-//     .WaitFor(rabbitmq);
+
+var typesenseApiKey = builder.Environment.IsDevelopment()
+    ? builder.Configuration["Parameters:typesense-api-key"]
+      ?? throw new InvalidOperationException("Could not get typesense api key")
+    : "${TYPESENSE_API_KEY}";
+
+var typesense = builder.AddContainer("typesense", "typesense/typesense", "29.0")
+    .WithArgs("--data-dir", "/data", "--api-key", typesenseApiKey, "--enable-cors")
+    .WithVolume("typesense-data", "/data")
+    .WithEnvironment("TYPESENSE_API_KEY", typesenseApiKey)
+    .WithHttpEndpoint(8108, 8108, name: "typesense");
+
+var typesenseContainer = typesense.GetEndpoint("typesense");
+
+var questionDb = postgres.AddDatabase("questionDb");
+
+var rabbitmq = builder.AddRabbitMQ("messaging")
+    .WithDataVolume("rabbitmq-data")
+    .WithManagementPlugin(port: 15672);
+
+var questionService = builder.AddProject<Projects.QuestionService>("question-svc")
+    .WithReference(keycloak)
+    .WithReference(questionDb)
+    .WithReference(rabbitmq)
+    .WaitFor(keycloak)
+    .WaitFor(questionDb)
+    .WaitFor(rabbitmq);
+
+
+var searchService = builder.AddProject<Projects.SearchService>("search-svc")
+    .WithEnvironment("typesense-api-key", typesenseApiKey)
+    .WithReference(typesenseContainer)
+    .WithReference(rabbitmq)
+    .WaitFor(typesense)
+    .WaitFor(rabbitmq);
+
+var yarp = builder.AddYarp("gateway")
+    .WithConfiguration(yarpBuilder =>
+    {
+        yarpBuilder.AddRoute("/questions/{**catch-all}", questionService);
+        yarpBuilder.AddRoute("/tags/{**catch-all}", questionService);
+        yarpBuilder.AddRoute("/search/{**catch-all}", searchService);
+    })
+    .WithEnvironment("ASPNETCORE_URLS", "http://*:8001")
+    .WithEndpoint(port: 8001, targetPort: 8001, scheme: "http", name: "gateway", isExternal: true)
+    .WithEnvironment("VIRTUAL_HOST", "api.overflow.local")
+    .WithEnvironment("VIRTUAL_PORT", "8001");
+
+var webapp = builder.AddNpmApp("webapp", "../webapp", "dev")
+    .WithReference(keycloak)
+    .WithHttpEndpoint(env: "PORT", port: 3000);
+
+if (!builder.Environment.IsDevelopment())
+{
+    builder.AddContainer("nginx-proxy", "nginxproxy/nginx-proxy", "1.8")
+        .WithEndpoint(80, 80, "nginx", isExternal: true)
+        .WithBindMount("/var/run/docker.sock", "/tmp/docker.sock", true);
+}
 
 builder.Build().Run();
