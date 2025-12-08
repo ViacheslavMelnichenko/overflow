74af60e08d2873a07670e7cbc02834d6e9441da1
Viacheslav Melnichenko <melnichenkoviatcheslav@gmail.com>
2025-12-08 04:56:42 +0100

    switch audience

--- webapp/src/components/nav/RegisterButton.tsx
+++ webapp/src/components/nav/RegisterButton.tsx
@@ -1,12 +1,12 @@
 import {Button} from "@heroui/button";
+import {authConfig} from "@/lib/config";
 
 export default function RegisterButton() {
-    const clientId = 'nextjs';
     const issuer = process.env.AUTH_KEYCLOAK_ISSUER;
     const redirectUrl = process.env.AUTH_URL;
     
     const registerUrl = `${issuer}/protocol/openid-connect/registrations` +
-        `?client_id=${clientId}&redirect_uri=` +
+        `?client_id=${authConfig.kcClientId}&redirect_uri=` +
         `${encodeURIComponent(redirectUrl!)}&response_type=code&scope=openid`;
     
     return (
